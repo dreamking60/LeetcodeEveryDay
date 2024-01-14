49e922d455a41018ddcd796748fe309bcc1a013f
dreamking60 <dreamlyboyczh@foxmail.com>
2024-01-14 04:39:50 +0000

    2024.01.14

---
+++ Java/Note.md
@@ -0,0 +1,24 @@
+## java switch case语句
+```java
+switch(expression){
+    case value :
+       //语句
+       break; //可选
+    case value :
+       //语句
+       break; //可选
+    //你可以有任意数量的case语句
+    default : //可选
+       //语句
+}
+```
+
+## Integer to String
+Three ways.
+```java
+Integer.toString()
+
+String.valueOf()
+
+String.format()
+```
---
+++ Java/fizzBuzz_412.java
@@ -0,0 +1,35 @@
+    public List<String> fizzBuzz(int n) {
+        List<String> ans = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            if((i+1) % 3 == 0) {
+                if((i+1) % 5 == 0) {
+                    ans.add("FizzBuzz");
+                } else {
+                    ans.add("Fizz");
+                }
+            } else if((i+1) % 5 == 0) {
+                ans.add("Buzz");
+            } else {
+                ans.add(String.valueOf(i+1));
+            }
+
+        }
+        return ans;
+    }
+
+    public List<String> fizzBuzz(int n) {
+        List<String> ans = new ArrayList<>();
+        for(int i = 1; i <= n; i++) {
+            if(i % 3 == 0 && i % 5 == 0) {
+                ans.add("FizzBuzz");
+            } else if(i % 3 == 0) {
+                ans.add("Fizz");
+            } else if(i % 5 == 0) {
+                ans.add("Buzz");
+            } else {
+                ans.add(String.valueOf(i));
+            }
+
+        }
+        return ans;
+    }
