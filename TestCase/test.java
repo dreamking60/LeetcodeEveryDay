45a6f8ac79e39a892e4f2cf40acfe70e72a78f30
dreamking60 <dreamlyboyczh@foxmail.com>
2024-01-28 17:19:50 +0800

    Weekly Contest 383

--- TestCase/test.java
+++ TestCase/test.java
@@ -1,5 +1,9 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Test Start:");
+        int n = Integer.parseInt(args[0]);
+        int m = Integer.parseInt(args[1]);
+        System.out.println(n & m);
+        System.out.println(n | m);
     }
 }
